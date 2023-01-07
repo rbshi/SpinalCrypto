44b3446183de1ec3f97fc7d77c207aefb0433bbd
rbshi <25062575+rbshi@users.noreply.github.com>
2023-01-07 13:59:33 +0100

    Update Version.scala

--- project/Version.scala
+++ project/Version.scala
@@ -1,6 +1,6 @@
 object CryptoVersion {
 
-  val scalaCompilers = Seq("2.11.12", "2.12.13", "2.13.6")
+  val scalaCompilers = Seq("2.11.12", "2.12.16", "2.13.6")
   val spinal        = "1.7.0a"
 
   private val version = "1.1.1"
